7cbca20837406d88b614f9333776b264490fcf8f
Emil Placheta <emil.minoru@gmail.com>
2020-02-23 15:38:10 +0000

    first commit

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,72 @@
+import React, { Fragment, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/ContactContext';
+
+export const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
+
+  function onLogout() {
+    logout();
+    clearContacts();
+  }
+
+  const authLinks = (
+    <Fragment>
+      <li>
+        Hello {user && user.name}
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <ul>
+        <li>
+          <Link to='/login'>Log In</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <Link to='/'>
+          <i className={icon} />
+        </Link>
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt'
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h4>About this App.</h4>
+      <p>Full Stack React App for keeping contacts by multiple users.</p>
+    </div>
+  );
+};
+
+export default About;

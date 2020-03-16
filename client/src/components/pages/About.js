440aacdbdea843b6c0bd7ef26bd1a7c8cb941759
Emil Placheta <emil.minoru@gmail.com>
2020-03-16 12:29:31 +0000

    About section updated

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -6,8 +6,15 @@ const About = () => {
       <h4>About this App</h4>
       <p>
         {' '}
-        React App build wth MERN stack. Allows cerating individual accounts with
-        their own property list.
+        Propertier is an app designed to help manage multiple properties, by
+        giving the user a brief overview of the current status and income
+        through the user's portfolio. It enables the user to have easy access to
+        checklists used when inspecting the property, or when downloading
+        documents e.g tenancy agreement. Thanks to use of Materialize, it
+        provides a familiar ‘Google’ look to its users, and great mobile
+        responsiveness. It's a full stack MERN app with complete back-end and
+        its own API. It utilises JSON web tokens to authenticate users and
+        validate accounts.
       </p>
     </div>
   );
